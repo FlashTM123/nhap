7df13766c5c35b48ef2465793de2bfefba5bbb8e
Trương Minh <154775327+FlashTM123@users.noreply.github.com>
2024-11-02 18:56:33 +0700

    Add files via upload

---
+++ baitap.sql
@@ -0,0 +1,250 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 02, 2024 at 12:50 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `baitap`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `C1` ()   BEGIN
+    SELECT employee_id, first_name, last_name, salary 
+    FROM employees 
+    ORDER BY salary ASC;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `C2` (IN `min_salary` DECIMAL(6,0), IN `max_salary` DECIMAL(6,0))   BEGIN
+    SELECT first_name, last_name FROM employees WHERE salary NOT BETWEEN min_salary AND max_salary;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `C3` ()   BEGIN
+  SELECT e.first_name, e.last_name FROM employees e 
+  INNER JOIN departments d ON e.department_id = d.department_id 
+  WHERE department_name = 'IT';
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `C4` ()   BEGIN
+  SELECT first_name,  last_name, MIN(salary) FROM employees;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `C5` ()   BEGIN
+  SELECT e.first_name, e.last_name, d.department_id, d.department_name FROM employees e INNER JOIN departments d ON e.department_id = d.department_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `C6` ()   BEGIN
+  SELECT department_name, COUNT(department_id) FROM employees 
+  JOIN departments USING (department_id) GROUP BY department_name;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries` (
+  `country_id` varchar(2) NOT NULL,
+  `country_name` varchar(40) NOT NULL,
+  `region_id` decimal(5,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `department_id` decimal(4,0) NOT NULL,
+  `department_name` varchar(30) NOT NULL,
+  `manager_id` decimal(6,0) DEFAULT NULL,
+  `location_id` decimal(4,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employee_id` decimal(6,0) NOT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `email` varchar(25) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `hire_date` date NOT NULL,
+  `job_id` varchar(10) DEFAULT NULL,
+  `salary` decimal(8,2) DEFAULT NULL,
+  `commission_pct` decimal(2,2) DEFAULT NULL,
+  `manager_id` decimal(6,0) DEFAULT NULL,
+  `department_id` decimal(4,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `job_id` varchar(10) NOT NULL,
+  `job_title` varchar(35) NOT NULL,
+  `min_salary` decimal(6,0) DEFAULT NULL,
+  `max_salary` decimal(6,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_history`
+--
+
+CREATE TABLE `job_history` (
+  `employee_id` decimal(6,0) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `job_id` varchar(10) DEFAULT NULL,
+  `department_id` decimal(4,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE `locations` (
+  `location_id` decimal(4,0) NOT NULL,
+  `street_address` varchar(40) DEFAULT NULL,
+  `postal_code` varchar(12) DEFAULT NULL,
+  `city` varchar(30) NOT NULL,
+  `state_province` varchar(25) DEFAULT NULL,
+  `country_id` varchar(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `regions`
+--
+
+CREATE TABLE `regions` (
+  `region_id` decimal(5,0) NOT NULL,
+  `region_name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`country_id`),
+  ADD KEY `region_id` (`region_id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`department_id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employee_id`),
+  ADD KEY `job_id` (`job_id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`job_id`);
+
+--
+-- Indexes for table `job_history`
+--
+ALTER TABLE `job_history`
+  ADD PRIMARY KEY (`employee_id`,`start_date`),
+  ADD KEY `job_id` (`job_id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`location_id`),
+  ADD KEY `country_id` (`country_id`);
+
+--
+-- Indexes for table `regions`
+--
+ALTER TABLE `regions`
+  ADD PRIMARY KEY (`region_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `countries`
+--
+ALTER TABLE `countries`
+  ADD CONSTRAINT `countries_ibfk_1` FOREIGN KEY (`region_id`) REFERENCES `regions` (`region_id`);
+
+--
+-- Constraints for table `departments`
+--
+ALTER TABLE `departments`
+  ADD CONSTRAINT `departments_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`);
+
+--
+-- Constraints for table `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`job_id`),
+  ADD CONSTRAINT `employees_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `departments` (`department_id`);
+
+--
+-- Constraints for table `job_history`
+--
+ALTER TABLE `job_history`
+  ADD CONSTRAINT `job_history_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`),
+  ADD CONSTRAINT `job_history_ibfk_2` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`job_id`),
+  ADD CONSTRAINT `job_history_ibfk_3` FOREIGN KEY (`department_id`) REFERENCES `departments` (`department_id`);
+
+--
+-- Constraints for table `locations`
+--
+ALTER TABLE `locations`
+  ADD CONSTRAINT `locations_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `countries` (`country_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
